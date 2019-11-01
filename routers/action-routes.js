789d413f06ae1be42e99e8b0cb46cbecb5b918b5
frescocodes <hueydakotah@gmail.com>
2019-11-01 13:41:58 -0400

    chore: remove action-routes, not necessary

--- routers/action-routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const actModel = require("../data/helpers/actionModel");
-const router = express.Router();
-const { get, insert, update, remove } = actModel;
-
-module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const projectRouter = require('./routers/project-routes')
 server.use(express.json());
 server.use(helmet());
 
-server.use('/api/projects/', projectRouter)
+server.use('/api/projects', projectRouter)
 
 server.get("/", (req, res) =>
   res.status(200).json({ message: "Server is running" })
